39d2bd632e9ea5c6205d1b4606e12a01b24c6927
hee <jkgt0521@naver.com>
2022-08-10 09:44:32 +0900

    android: listView

--- hyggeproject/app/src/main/java/com/hygge/hygge/MainActivity.kt
+++ hyggeproject/app/src/main/java/com/hygge/hygge/MainActivity.kt
@@ -3,10 +3,23 @@ package com.hygge.hygge
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ListView
+import com.hygge.hygge.shelter_list.Shelter
+import com.hygge.hygge.shelter_list.ShelterListCustomAdapter
 
 class MainActivity : AppCompatActivity() {
+
+    val shelterList = arrayListOf<Shelter>(
+        Shelter("쉼터1", "안녕하숑", "tmp1"),
+        Shelter("쉼터2", "반가워요", "tmp_image")
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.shelter_list_view)
+
+        val customAdapter = ShelterListCustomAdapter(this, shelterList)
+        val listView = findViewById<ListView>(R.id.shelter_list_view)
+        listView.adapter = customAdapter
     }
 }
--- hyggeproject/app/src/main/java/com/hygge/hygge/shelter_list/ShelterListCustomAdapter.kt
+++ hyggeproject/app/src/main/java/com/hygge/hygge/shelter_list/ShelterListCustomAdapter.kt
@@ -11,15 +11,15 @@ import com.hygge.hygge.R
 
 class ShelterListCustomAdapter(val context: Context, val shelterList: ArrayList<Shelter>) :  BaseAdapter() {
     override fun getCount(): Int {    //ListView에 속한 아이템 전체 수를 반환함
-        TODO("Not yet implemented")
+        return shelterList.size
     }
 
     override fun getItem(p0: Int): Any {   //해당 위치의 item id를 반환
-        TODO("Not yet implemented")
+        return p0
     }
 
     override fun getItemId(p0: Int): Long {   //해당 위치의 아이템을 반환
-        TODO("Not yet implemented")
+        return 0
     }
 
     override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {   //View와 데이터를 연결
@@ -30,7 +30,12 @@ class ShelterListCustomAdapter(val context: Context, val shelterList: ArrayList<
         val shDescription = view.findViewById<TextView>(R.id.shelter_description)
 
         val shelter = shelterList[p0]
-        val photoId = context.resources.getIdentifier()
+        val photoId = context.resources.getIdentifier(shelter.photo, "drawable", context.packageName)
 
+        shImage.setImageResource(photoId)
+        shTitle.text = shelter.title
+        shDescription.text = shelter.description
+
+        return view
     }
 }
